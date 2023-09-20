a1f6a673495aabdf87448119baab509ad6cd9e69
rabopuk <rabopuk@gmail.com>
2023-09-20 15:16:37 +0400

    add: id generation, total price refreshing, serialize form

--- script.js
+++ script.js
@@ -2,22 +2,30 @@
 /* eslint-disable max-len */
 'use strict';
 
-// В проекте CMS у элемента с классом overlay уберите класс active
 const overlay = document.querySelector('.overlay');
+const addBtn = document.querySelector('.panel__add-goods');
+const tableBody = document.querySelector('.table__body');
+const discountCheckbox = document.getElementById('discount');
+const discountCountInput = document.querySelector('.modal__input_discount');
+const countInput = document.getElementById('count');
+const priceInput = document.getElementById('price');
+const modalForm = document.querySelector('.modal__form');
+
+
+// В проекте CMS у элемента с классом overlay уберите класс active
 overlay.classList.remove('active');
 
-// Создайте функцию createRow, которая будет получать объект и на основе объекта формировать элемент
-const tbody = document.querySelector('.table__body');
 
-const createRow = (element, items) => {
+// Создайте функцию createRow, которая будет получать объект и на основе объекта формировать элемент
+const createRow = (items) => {
   const newRow = document.createElement('tr');
-  const { name, category, units, count, price } = items;
+  const { id, name, category, units, count, price } = items;
   newRow.classList.add('item');
 
   newRow.innerHTML = `
-    <td class="table__cell">-</td>
+    <td class="table__cell">${id}</td>
     <td class="table__cell table__cell_left table__cell_name">
-      <span class="table__cell-id">id: 24601654816512</span>
+      <span class="table__cell-id">id: ${id}</span>
       ${name}
     </td>
     <td class="table__cell table__cell_left">${category}</td>
@@ -35,53 +43,33 @@ const createRow = (element, items) => {
   return newRow;
 };
 
-// Функция для сериализации формы в объект FormData
-const serializeForm = form => {
-  const { elements } = form;
-  const data = new FormData();
 
-  Array.from(elements)
-    .filter(item => !!item.name)
-    .forEach(element => {
-      const { name, type } = element;
-      const value = type === 'checkbox' ? element.checked : element.value;
+// Обновление итоговой стоимости над таблицей
+const updateTotalPriceAboveTable = () => {
+  // Все строки товаров
+  const rows = document.querySelectorAll('.item');
 
-      data.append(name, value);
-    });
+  // Сумма
+  let total = 0;
 
-  return data;
+  // Вычислить сумму
+  rows.forEach(row => {
+    const count = parseInt(row.querySelector('.table__cell:nth-child(5)').textContent);
+    const price = parseInt(row.querySelector('.table__cell:nth-child(6)').textContent.replace('$', '')) || 0;
+    total += count * price;
+  });
+
+  // Обновить текст итоговой стоимости над таблицей
+  const totalAboveTable = document.querySelector('.cms__total-price');
+  totalAboveTable.textContent = `$ ${total.toFixed(2)}`;
 };
 
-// Создайте функцию renderGoods, принимает один параметр массив с объектами
-// Функция renderGoods перебирает массив и вставляет строки, созданные на основе createRow, в таблицу
-const renderGoods = itemsArray => {
-  for (const item of itemsArray) {
-    createRow(tbody, item);
-  }
-};
-// Пример данных
-const itemsData = [
-  {
-    id: 5,
-    dataId: 132123412,
-    name: 'Навигационная система Soundmax Turbo',
-    category: 'Техника для дома',
-    unit: 'шт',
-    quantity: 7,
-    price: 150,
-    total() {
-      return this.quantity * this.price;
-    },
-  },
-];
-
-renderGoods(itemsData);
 
 // При нажатии на кнопку "Добавить товар", открывать модальное окно
-const addBtn = document.querySelector('.panel__add-goods');
 addBtn.addEventListener('click', () => {
   overlay.classList.add('active');
 });
+
 
 // Закрывать модальное окно без использования методов stopImmediatePropagation и stopPropagation
 overlay.addEventListener('click', e => {
@@ -92,11 +80,8 @@ overlay.addEventListener('click', e => {
   }
 });
 
-// При клике на кнопку удалить в таблице, удалять строку из вёрстки и объект из базы данных
-// В консоль выводить базу данных после удаления поля
-const tableBody = document.querySelector('.table__body');
-const tRows = tableBody.querySelectorAll('.item');
 
+// При клике на кнопку удалить в таблице, удалять строку из вёрстки и объект из базы данных
 tableBody.addEventListener('click', e => {
   const target = e.target;
 
@@ -106,48 +91,94 @@ tableBody.addEventListener('click', e => {
     if (item) {
       item.remove();
 
-      const newRows = [...tRows].filter(row => row !== item);
-      console.log('БД: ', [...newRows]);
+      // Все строки таблицы после удаления товара
+      const newRows = Array.from(tableBody.querySelectorAll('.item'));
+
+      // В консоль выводить базу данных после удаления поля
+      const tableData = newRows.map(row => {
+        const rowData = Array.from(row.cells).map(cell => cell.textContent);
+        return rowData;
+      });
+
+      console.log('БД: ', tableData);
+
+      // Обновление итоговой стоимости над таблицей после удаления товара
+      updateTotalPriceAboveTable();
     }
   }
 });
 
-// 1. В форме если поставить чекбокс должен быть разблокирован input с name discount_count
-// 2. Если чекбокс убрать поле discount_count очищается и блокируется
-const discountCheckbox = document.getElementById('discount');
-const discountCountInput = document.querySelector('.modal__input_discount');
 
-// Изменения состояния чекбокса
+// В форме если поставить чекбокс должен быть разблокирован input с name discount_count
 discountCheckbox.addEventListener('change', () => {
   // Если чекбокс отмечен, разблокировать поле ввода
   if (discountCheckbox.checked) {
     discountCountInput.disabled = false;
   } else {
-    // Заблокировать поле ввода и очистить его
-    discountCountInput.disabled = true;
+    // Если чекбокс убрать, поле discount_count очищается и блокируется
     discountCountInput.value = '';
+    discountCountInput.disabled = true;
   }
 });
 
-// 3. Реализовать добавление нового товара из формы в таблицу
-const modalForm = document.querySelector('.modal__form');
+
+// Генератор случайного ID
+const generateRandomId = () => Math.floor(Math.random() * 1000000000000000);
+
+
+// Сериализация формы в объект FormData
+const serializeForm = form => {
+  const { elements } = form;
+  const data = new FormData();
+
+  Array.from(elements)
+    .filter(item => !!item.name)
+    .forEach(element => {
+      const { name } = element;
+      const value = element.value;
+
+      data.append(name, value);
+    });
+
+  return data;
+};
+
+
+// Пересчёт итоговой стоимости в модальном окне
+const recalculateTotalPrice = () => {
+  // Значения из полей Количество и Цена
+  const count = parseInt(countInput.value) || 0;
+  const price = parseInt(priceInput.value) || 0;
+
+  // Итоговая стоимость
+  const total = count * price;
+
+  // Обновить текст итоговой стоимости в модальном окне
+  const modalTotalPrice = document.querySelector('.modal__total-price');
+  modalTotalPrice.textContent = `${total.toFixed(2)}`;
+};
+// Слушатели событий изменения полей Количество и Цена
+countInput.addEventListener('input', recalculateTotalPrice);
+priceInput.addEventListener('input', recalculateTotalPrice);
+
 
 // Обработчик события отправки формы
 modalForm.addEventListener('submit', e => {
   e.preventDefault();
 
-  // Используем функцию для сериализации формы
+  // Сериализация формы
   const formData = serializeForm(modalForm);
 
-  // Новая строка для таблицы
-  const newRow = createRow(tbody, Object.fromEntries(formData.entries()));
+  // Случайный ID для нового товара
+  const newItem = { id: generateRandomId(), ...Object.fromEntries(formData.entries()) };
 
+  // Новая строка для таблицы
+  const newRow = createRow(newItem);
   // Текущее количество строк в таблице
   const rowCount = tableBody.rows.length;
 
   // Установить номер строки в первой ячейке
   newRow.querySelector('.table__cell:first-child').textContent = rowCount + 1;
-
   // Добавить новую строку в таблицу
   tableBody.append(newRow);
 
@@ -155,15 +186,55 @@ modalForm.addEventListener('submit', e => {
   modalForm.reset();
   overlay.classList.remove('active');
 
-  console.log('formData: ', Object.fromEntries(formData.entries()));
+  // Обновление итоговой стоимости над таблицей после добавления товара
+  updateTotalPriceAboveTable();
+
+  const formDataArr = Array.from(formData.entries());
+
+  // Данные строки
+  console.log('formData: ', formDataArr);
 });
 
-// 4. Все поля обязательны к заполнению(исключение изображение), можно реализовать с помощью атрибутов в html
 
-// 5. Укажите правильный тип полей в html, для текста - text, для чисел - number
+// Создайте функцию renderGoods, принимает один параметр массив с объектами
+// Функция renderGoods перебирает массив и вставляет строки, созданные на основе createRow, в таблицу
+const renderGoods = itemsArray => {
+  for (const item of itemsArray) {
+    item.id = generateRandomId(); // случайный ID
+    createRow(item);
+  }
 
-// 6. При открытии модального окна должен генерироваться случайный id и заполняться span с классом vendor - code__id
+  // Обновление итоговой стоимости над таблицей после рендеринга товаров
+  updateTotalPriceAboveTable();
+};
+// Пример данных
+const itemsData = [
+  {
+    name: 'Навигационная система Soundmax Turbo',
+    category: 'Техника для дома',
+    units: 'шт',
+    count: 7,
+    price: 150,
+  },
+];
+// Отрендерить товары
+renderGoods(itemsData);
 
-// 7. Итоговая стоимость в модальном окне должна правильно высчитываться при смене фокуса
 
-// 8. Итоговая стоимость над таблицей должна корректно отображать сумму всех товаров
+// Обновление итоговой стоимости после загрузки страницы
+window.addEventListener('load', updateTotalPriceAboveTable);
+
+
+// Удаление строки и обновление итоговой стоимости после удаления товара
+tableBody.addEventListener('click', e => {
+  const target = e.target;
+
+  if (target.classList.contains('table__btn_del')) {
+    const item = target.closest('.item');
+
+    if (item) {
+      item.remove();
+      updateTotalPriceAboveTable();
+    }
+  }
+});
