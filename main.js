868991aa0e39f6551d9cc5a6b9563c95ed069b93
rabopuk <rabopuk@gmail.com>
2024-02-05 15:32:04 +0400

    fix

--- main.js
+++ main.js
@@ -1,88 +1,16 @@
 /* eslint-disable object-curly-spacing */
 import { fetchData } from './modules/APIUtils.js';
-import {
-  addRowToTable,
-  getDOMElements,
-  renderGoods,
-  updateModalTotalPrice,
-  updateRowNumbers,
-  updateTotalPrice,
-} from './modules/DOMUtils.js';
-import {
-  addToDatabase,
-  generateRandomId,
-  serializeForm,
-} from './modules/dataUtils.js';
-import { handleDeleteButtonClick } from './modules/eventHandlers.js';
+import { getDOMElements, renderGoods } from './modules/DOMUtils.js';
+import { addEventListeners } from './modules/control.js';
 
-const init = () => {
-  let database = [];
-  const { overlay, addBtn, tableBody, discountCheckbox, discountCountInput, modalForm, countInput, priceInput, vendorCodeIdSpan } = getDOMElements();
+const init = async () => {
+  const database = await fetchData();
+  const domElements = getDOMElements();
 
-  overlay.classList.remove('active');
+  domElements.overlay.classList.remove('active');
 
-  addBtn.addEventListener('click', () => {
-    const newId = generateRandomId();
-    vendorCodeIdSpan.textContent = `id: ${newId}`;
-    overlay.classList.add('active');
-  });
-
-  overlay.addEventListener('click', (e) => {
-    const target = e.target;
-
-    if (target === overlay || target.closest('.modal__close')) {
-      overlay.classList.remove('active');
-    }
-  });
-
-  tableBody.addEventListener('click', (e) => {
-    const target = e.target;
-
-    if (target.classList.contains('table__btn_del')) {
-      const item = target.closest('.item');
-
-      if (item) {
-        database = handleDeleteButtonClick(item, database);
-      }
-    }
-  });
-
-  discountCheckbox.addEventListener('change', () => {
-    discountCountInput.disabled = !discountCheckbox.checked;
-  });
-
-  modalForm.addEventListener('submit', async (e) => {
-    e.preventDefault();
-
-    const newId = parseInt(vendorCodeIdSpan.textContent.replace('id: ', ''));
-    const formData = serializeForm(e.target);
-
-    const count = parseInt(formData.get('count')) || 0;
-    const price = parseFloat(formData.get('price')) || 0;
-
-    const { newItem, newDb } = addToDatabase(newId, formData.get('name'), formData.get('category'), formData.get('units'), count, price, database);
-    addRowToTable(newItem);
-
-    console.log(newDb);
-    console.log(newItem);
-
-    modalForm.reset();
-    overlay.classList.remove('active');
-
-    const updatedRows = updateRowNumbers(tableBody.querySelectorAll('.item'));
-    updateTotalPrice(updatedRows);
-
-    database = newDb;
-  });
-
-  countInput.addEventListener('input', () => updateModalTotalPrice(countInput, priceInput));
-
-  priceInput.addEventListener('input', () => updateModalTotalPrice(countInput, priceInput));
-
-  document.addEventListener('DOMContentLoaded', async () => {
-    database = await fetchData();
-    renderGoods(database);
-  });
+  addEventListeners(database, ...Object.values(domElements));
+  renderGoods(database);
 };
 
 init();
---
+++ modules/control.js
@@ -0,0 +1,87 @@
+/* eslint-disable indent */
+/* eslint-disable object-curly-spacing */
+import { fetchData } from './APIUtils.js';
+import {
+  addRowToTable,
+  renderGoods,
+  updateModalTotalPrice,
+  updateRowNumbers,
+  updateTotalPrice,
+} from './DOMUtils.js';
+import { addToDatabase, generateRandomId, serializeForm } from './dataUtils.js';
+import { handleDeleteButtonClick } from './eventHandlers.js';
+
+export const addEventListeners = (
+  database,
+  overlay,
+  addBtn,
+  tableBody,
+  discountCheckbox,
+  discountCountInput,
+  modalForm,
+  countInput,
+  priceInput,
+  vendorCodeIdSpan,
+) => {
+  addBtn.addEventListener('click', () => {
+    const newId = generateRandomId();
+    vendorCodeIdSpan.textContent = `id: ${newId}`;
+    overlay.classList.add('active');
+  });
+
+  overlay.addEventListener('click', (e) => {
+    const target = e.target;
+
+    if (target === overlay || target.closest('.modal__close')) {
+      overlay.classList.remove('active');
+    }
+  });
+
+  tableBody.addEventListener('click', (e) => {
+    const target = e.target;
+
+    if (target.classList.contains('table__btn_del')) {
+      const item = target.closest('.item');
+
+      if (item) {
+        database = handleDeleteButtonClick(item, database);
+      }
+    }
+  });
+
+  discountCheckbox.addEventListener('change', () => {
+    discountCountInput.disabled = !discountCheckbox.checked;
+  });
+
+  modalForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const newId = parseInt(vendorCodeIdSpan.textContent.replace('id: ', ''));
+    const formData = serializeForm(e.target);
+
+    const count = parseInt(formData.get('count')) || 0;
+    const price = parseFloat(formData.get('price')) || 0;
+
+    const { newItem, newDb } = addToDatabase(newId, formData.get('name'), formData.get('category'), formData.get('units'), count, price, database);
+    addRowToTable(newItem);
+
+    modalForm.reset();
+    overlay.classList.remove('active');
+
+    const updatedRows = updateRowNumbers(tableBody.querySelectorAll('.item'));
+    updateTotalPrice(updatedRows);
+
+    database = newDb;
+  });
+
+  countInput.addEventListener('input', () =>
+    updateModalTotalPrice(countInput, priceInput));
+
+  priceInput.addEventListener('input', () =>
+    updateModalTotalPrice(countInput, priceInput));
+
+  document.addEventListener('DOMContentLoaded', async () => {
+    database = await fetchData();
+    renderGoods(database);
+  });
+};
